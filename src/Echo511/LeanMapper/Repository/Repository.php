de478670795fe9438c531e29ab2e8821932c6d48
Nikolas Tsiongas <ntsiongas@gmail.com>
2014-06-11 19:00:02 +0200

    Added annotations

--- src/Echo511/LeanMapper/Repository/Repository.php
+++ src/Echo511/LeanMapper/Repository/Repository.php
@@ -6,8 +6,14 @@ use LeanMapper\Connection;
 use LeanMapper\IEntityFactory;
 use LeanMapper\IMapper;
 use LeanMapper\Repository;
+use LeanQuery\DomainQuery;
 use LeanQuery\DomainQueryFactory;
+use Nette\Reflection\ClassType;
 
+/**
+ * Repository enhanced with LeanQuery.
+ * @author Nikolas Tsiongas
+ */
 abstract class AbstractRepository extends Repository
 {
 
@@ -22,9 +28,14 @@ abstract class AbstractRepository extends Repository
 
 
 
+	/**
+	 * Create domain query, select entity of repository with alias
+	 * @param string $alias
+	 * @return DomainQuery
+	 */
 	protected function createQuery($alias)
 	{
-		$repositoryClass = (new \Nette\Reflection\ClassType($this))->getName();
+		$repositoryClass = (new ClassType($this))->getName();
 		return $this->queryFactory->createQuery()->select($alias)->from($this->mapper->getEntityClass($this->mapper->getTableByRepositoryClass($repositoryClass)), $alias);
 	}
 
